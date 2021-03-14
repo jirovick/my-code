89ecc6626f578d655a6f6b985f09bc0020fb443d
Vanya <vanya.ushakov1990@gmail.com>
2021-03-14 21:46:05 +0300

    init

---
+++ task 2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] array = new int[12];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(0, 12);
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+            int min = array[0];
+            for (int i = 0; i < 12; i++)
+
+            {
+                if (array[i] < min)
+                {
+                    min = array[i];
+                }
+            }
+
+            int max = array[11];
+            for (int i = 0; i < 12; i++)
+            {
+                if (array[i] > max)
+                {
+                    max = array[i];
+                }
+            }
+
+            Console.WriteLine("минимальное {0}", min);
+            Console.WriteLine("максимальное {0}", max);
+            Console.Read();
+        }
+    }
+}
