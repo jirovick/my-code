52c88d94186082f52602a066a8c56afb5a260472
Vanya <vanya.ushakov1990@gmail.com>
2021-03-25 22:56:56 +0300

    switch calculator

---
+++ задание 2/Calculator.cs
@@ -0,0 +1,58 @@
+﻿namespace задание_2
+{
+    public class Calculator
+    {
+        /// <summary>
+        /// суммирует числа
+        /// </summary>
+        /// <param name="number1">первое число</param>
+        /// <param name="number2">второе число</param>
+        /// <returns></returns>
+        public static double CountPlus(double number1, double number2)
+        {
+            double resault;
+            resault = number1 + number2;
+            return resault;
+          
+        }
+
+        /// <summary>
+        /// вычитает числа
+        /// </summary>
+        /// <param name="number1">первое число</param>
+        /// <param name="number2">второе число</param>
+        /// <returns></returns>
+        public static double CountMinus (double number1, double number2)
+        {
+            double resault;
+            resault = number1 - number2;
+            return resault;
+        }
+
+        /// <summary>
+        /// умножает числа
+        /// </summary>
+        /// <param name="number1">первое число</param>
+        /// <param name="number2">второе число</param>
+        /// <returns></returns>
+        public static double CountTimes(double number1, double number2)
+        {
+            double resault;
+            resault = number1 * number2;
+            return resault;
+        }
+
+        /// <summary>
+        /// делит числа
+        /// </summary>
+        /// <param name="number1">первое число</param>
+        /// <param name="number2">второе число</param>
+        /// <returns></returns>
+        public static double CountDiv(double number1, double number2)
+        {
+            double resault;
+            resault = number1 / number2;
+            return resault;
+        }
+    }
+}
