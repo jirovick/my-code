7b9dc83e41e42d3c5e5388305fa7a0e93f5f9521
Vanya <vanya.ushakov1990@gmail.com>
2021-03-28 15:50:11 +0300

    calculator

--- задание 2/Calculator.cs
+++ задание 2/Calculator.cs
@@ -1,4 +1,6 @@
-﻿namespace задание_2
+﻿using System;
+
+namespace задание_2
 {
     public class Calculator
     {
@@ -52,6 +54,39 @@
         {
             double resault;
             resault = number1 / number2;
+            return resault;
+        }
+        /// <summary>
+        /// находит тангенс угла
+        /// </summary>
+        /// <param name="number1">первое число</param>
+        /// <returns></returns>
+        public static double CountTan(double number1)
+        {
+            double resault;
+            resault = Math.Tan(number1);
+            return resault;
+        }
+        /// <summary>
+        /// находит косинус угла
+        /// </summary>
+        /// <param name="number1">первое число</param>
+        /// <returns></returns>
+        public static double CountCos(double number1)
+        {
+            double resault;
+            resault = Math.Cos(number1);
+            return resault;
+        }
+        /// <summary>
+        /// находит синус угла
+        /// </summary>
+        /// <param name="number1">первое число</param>
+        /// <returns></returns>
+        public static double CountSin(double number1)
+        {
+            double resault;
+            resault = Math.Sin(number1);
             return resault;
         }
     }
